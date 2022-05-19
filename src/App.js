5f78f81a2c7b77ce74f1104daf1c4306b468a2da
Jullia Layne <83183478+jullialayne@users.noreply.github.com>
2022-05-19 13:48:03 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,15 +2,15 @@ import React from "react";
 import Layout from "./components/layout";
 import NoSearch from "./components/no-search";
 import Profile from "./components/profile";
-import useUser from "./hooks/github-hooks";
+import useGithub from "./hooks/github-hooks"; 
 
 const App = () => {
-  const { userState } = useUser();
+  const { githubState } = useGithub();
   return (
     <Layout>
-      {userState.hasUser ? (
+      {githubState.hasUser ? (
         <>
-          {userState.loading ? (
+          {githubState.loading ? (
             <p>Loading</p>
           ) : (
             <>

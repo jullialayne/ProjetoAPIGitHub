01d08f58932e9f5449851d7c74b4fc3a2bcdf51f
Jullia Layne <83183478+jullialayne@users.noreply.github.com>
2022-05-30 18:05:09 -0300

    Add files via upload

---
+++ states-e-effects/src/Components/App/App.jsx
@@ -0,0 +1,14 @@
+ 
+import IfoodCounter from '../IfoodCounter/IfoodCounter';
+import './App.css'; 
+function App() {
+  return (
+    <>
+      <h1>Ifood</h1>
+      <IfoodCounter />
+
+    </>
+  );
+}
+
+export default App;
